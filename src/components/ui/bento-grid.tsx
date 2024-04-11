0cfdaaa6dc73880ca8f8bc2fdf300da66bea7676
bharathkumar369 <bharathpathapati@gmail.com>
2024-04-11 13:20:04 +0530

    project animations removed

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -13,7 +13,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[20rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid mt-[150px] sm:mt-[200px] md:auto-rows-[20rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
         className
       )}
     >
@@ -41,13 +41,6 @@ export const BentoGridItem = ({
 }) => {
   return (
     <motion.div
-    initial={{ opacity: 0.0, y: -40 }}
-    whileInView={{ opacity: 1, y: 150}}
-    transition={{
-      delay: 0.3,
-      duration: 2.5,
-      ease: "easeInOut",
-    }}
       className={cn(
         "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-myOrange bg-white border border-transparent justify-between flex flex-col space-y-4",
         className,
